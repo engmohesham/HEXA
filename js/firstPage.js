752ebc1859209ed260801ca1c9ae67aa5652e0be
Mohamed Hesham <68177072+engmohesham@users.noreply.github.com>
2022-08-21 17:10:58 +0200

    Update firstPage.js

--- js/firstPage.js
+++ js/firstPage.js
@@ -13,16 +13,16 @@ function showSettings() {
 		settings.style.zIndex = '2'
 		document.querySelector('#show-settings').style.boxShadow = 'none'
 		settings.style.boxShadow = '-0.2em 0.2em 0.7em -0.4em hsl(0, 0%, 0%)'
-		settings.style.height = '19.5em'
-		settings.style.width = '35em'
-		document.querySelector('.settings-head span').style.color = 'hsl(0,90%,42%)'
+		settings.style.height = '18em'
+		settings.style.width = '48em'
+		document.querySelector('.settings-head span').style.color = '#fff'
 	}, 125)
 }
 
 function hideSettings() {
 	var settings = document.querySelector('.settings')
 	document.querySelector('.settings-head span').style.color = 'hsl(220, 16%, 18%)'
-	settings.style.height = '2.3em'
+	settings.style.height = '2em'
 	settings.style.width = '6.9em'
 	setTimeout(function () {
 		settings.style.zIndex = '0'
@@ -33,11 +33,11 @@ function hideSettings() {
 }
 
 
-function changeColours() {
-	document.querySelector('#sec1').style.background = 'hsl(' + (hue) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
-	document.querySelector('#sec2').style.background = 'hsl(' + (hue + variance) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
-	document.querySelector('#sec3').style.background = 'hsl(' + (hue + variance * 2) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
-}
+// function changeColours() {
+// 	document.querySelector('#sec1').style.background = 'hsl(' + (hue) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
+// 	document.querySelector('#sec2').style.background = 'hsl(' + (hue + variance) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
+// 	document.querySelector('#sec3').style.background = 'hsl(' + (hue + variance * 2) + ',' + saturation + '%, ' + lightness + '%, ' + opacity + '%)'
+// }
 
 
 let svg_img = document.querySelectorAll('.svg-img')
@@ -74,12 +74,13 @@ svg_img[2].addEventListener('click', function () {
 })
 
 document.addEventListener("mousemove", parallax);
-function parallax(e){
+
+function parallax(e) {
 	this.querySelectorAll('.layer').forEach(layer => {
 		const speed = layer.getAttribute('data-speed')
 
-		const x = (window.innerWidth - e.pageX*speed)/100
-		const y = (window.innerHeight - e.pageY*speed)/100
+		const x = (window.innerWidth - e.pageX * speed) / 100
+		const y = (window.innerHeight - e.pageY * speed) / 100
 
 		layer.style.transform = `translateX(${x}px) translateY(${y}px)`
 	})
